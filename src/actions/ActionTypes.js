3cb554280ed4456a5c0312549d99bfaca1ebf31c
HongJirr <ghdtnsgus2!>
2020-05-04 17:30:57 +0900

    React-redux 공부

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,4 @@
+export const INCREMENT = 'INCREMENT';
+export const DECEMENT = 'DECREMENT';
+export const SET_COLOR = 'SET_COLOR';
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore( reducers );
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
