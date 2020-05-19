3b57fa89d35f466664f044e32755bbd158ad3844
HongJirr <gozlsrmawl@naver.com>
2020-05-19 16:45:09 +0900

    splice -> slice 수정

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -22,45 +22,45 @@ function counter( state = initialState, action){
                   number: 0
                }
             ]
-         }
+         };
          case types.INCREMENT :
             console.log("...counters",counters[action.index].number);
             console.log("action.index",action.index);
             return{
                counters: [
-                  ...counters.splice(0,action.index),
+                  ...counters.slice(0,action.index),
                   {
                      ...counters[action.index],
                      number: counters[action.index].number +1
                   },
-                  ...counters.splice(action.index+1, counters.length)
+                  ...counters.slice(action.index+1, counters.length)
                ]
             }
             case types.DECEMENT :
                return{
                   counters: [
-                     ...counters.splice(0,action.index),
+                     ...counters.slice(0,action.index),
                      {
                         ...counters[action.index],
                         number: counters[action.index].number -1
                      },
-                     ...counters.splice(action.index+1, counters.length)
+                     ...counters.slice(action.index+1, counters.length)
                   ]
                }
                case types.SET_COLOR :
                   return{
                      counters: [
-                        ...counters.splice(0,action.index),
+                        ...counters.slice(0,action.index),
                         {
                            ...counters[action.index],
                            color: action.color
                         },
-                        ...counters.splice(action.index+1, counters.length)
+                        ...counters.slice(action.index+1, counters.length)
                      ]
                   }
                   case types.REMOVE :
                      return{
-                        counters: counters.splice(0,counters.length-1)
+                        counters: counters.slice(0,counters.length-1)
                      }
                      default:
                         return state;
