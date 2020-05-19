b021db286a6ddcc55149b31776637cb497813136
HongJirr <gozlsrmawl@naver.com>
2020-05-19 16:36:32 +0900

    멀티카운터로 수정중..

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,4 +1,5 @@
 export const INCREMENT = 'INCREMENT';
 export const DECEMENT = 'DECREMENT';
 export const SET_COLOR = 'SET_COLOR';
-
+export const CREATE = 'CREATE';
+export const REMOVE = 'REMOVE';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,70 @@
-import number from './number';
-import color from './color';
+import * as types from '../actions/ActionTypes';
 
-import { combineReducers } from 'redux';
+const initialState = {
+   counters: [
+      {
+         color: 'black',
+         number: 0
+      }
+   ]
+};
 
-const reducer = combineReducers({
-   numberData: number,
-   colorData: color
-});
+function counter( state = initialState, action){
+   const { counters } = state;
 
-export default reducer;
+   switch(action.type){
+      case types.CREATE : 
+         return {
+            counters: [
+               ...counters,
+               {
+                  color: action.color,
+                  number: 0
+               }
+            ]
+         }
+         case types.INCREMENT :
+            console.log("...counters",counters[action.index].number);
+            console.log("action.index",action.index);
+            return{
+               counters: [
+                  ...counters.splice(0,action.index),
+                  {
+                     ...counters[action.index],
+                     number: counters[action.index].number +1
+                  },
+                  ...counters.splice(action.index+1, counters.length)
+               ]
+            }
+            case types.DECEMENT :
+               return{
+                  counters: [
+                     ...counters.splice(0,action.index),
+                     {
+                        ...counters[action.index],
+                        number: counters[action.index].number -1
+                     },
+                     ...counters.splice(action.index+1, counters.length)
+                  ]
+               }
+               case types.SET_COLOR :
+                  return{
+                     counters: [
+                        ...counters.splice(0,action.index),
+                        {
+                           ...counters[action.index],
+                           color: action.color
+                        },
+                        ...counters.splice(action.index+1, counters.length)
+                     ]
+                  }
+                  case types.REMOVE :
+                     return{
+                        counters: counters.splice(0,counters.length-1)
+                     }
+                     default:
+                        return state;
+   }
+}
+
+export default counter;
